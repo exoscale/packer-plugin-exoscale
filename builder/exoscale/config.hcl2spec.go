28419b2b3d1a104c3e5f8f1f4eb3f3584ea182c0
Cédric Dufour <cedric.dufour@exoscale.ch>
2022-06-02 14:01:40 +0200

    Add support for Build, Maintainer and Version attributes

--- builder/exoscale/config.hcl2spec.go
+++ builder/exoscale/config.hcl2spec.go
@@ -30,6 +30,9 @@ type FlatConfig struct {
 	TemplateBootMode           *string           `mapstructure:"template_boot_mode" cty:"template_boot_mode" hcl:"template_boot_mode"`
 	TemplateDisablePassword    *bool             `mapstructure:"template_disable_password" cty:"template_disable_password" hcl:"template_disable_password"`
 	TemplateDisableSSHKey      *bool             `mapstructure:"template_disable_sshkey" cty:"template_disable_sshkey" hcl:"template_disable_sshkey"`
+	TemplateMaintainer         *string           `mapstructure:"template_maintainer" cty:"template_maintainer" hcl:"template_maintainer"`
+	TemplateVersion            *string           `mapstructure:"template_version" cty:"template_version" hcl:"template_version"`
+	TemplateBuild              *string           `mapstructure:"template_build" cty:"template_build" hcl:"template_build"`
 	TemplateZone               *string           `mapstructure:"template_zone" cty:"template_zone" hcl:"template_zone"`
 	PackerBuildName            *string           `mapstructure:"packer_build_name" cty:"packer_build_name" hcl:"packer_build_name"`
 	PackerBuilderType          *string           `mapstructure:"packer_builder_type" cty:"packer_builder_type" hcl:"packer_builder_type"`
@@ -122,6 +125,9 @@ func (*FlatConfig) HCL2Spec() map[string]hcldec.Spec {
 		"template_boot_mode":           &hcldec.AttrSpec{Name: "template_boot_mode", Type: cty.String, Required: false},
 		"template_disable_password":    &hcldec.AttrSpec{Name: "template_disable_password", Type: cty.Bool, Required: false},
 		"template_disable_sshkey":      &hcldec.AttrSpec{Name: "template_disable_sshkey", Type: cty.Bool, Required: false},
+		"template_maintainer":          &hcldec.AttrSpec{Name: "template_maintainer", Type: cty.String, Required: false},
+		"template_version":             &hcldec.AttrSpec{Name: "template_version", Type: cty.String, Required: false},
+		"template_build":               &hcldec.AttrSpec{Name: "template_build", Type: cty.String, Required: false},
 		"template_zone":                &hcldec.AttrSpec{Name: "template_zone", Type: cty.String, Required: false},
 		"packer_build_name":            &hcldec.AttrSpec{Name: "packer_build_name", Type: cty.String, Required: false},
 		"packer_builder_type":          &hcldec.AttrSpec{Name: "packer_builder_type", Type: cty.String, Required: false},
--- builder/exoscale/step_register_template_test.go
+++ builder/exoscale/step_register_template_test.go
@@ -17,6 +17,9 @@ func (ts *testSuite) TestStepRegisterTemplate_Run() {
 			TemplateDescription: ts.randomString(10),
 			TemplateUsername:    ts.randomString(10),
 			TemplateBootMode:    ts.randomString(10),
+			TemplateMaintainer:  ts.randomString(10),
+			TemplateVersion:     ts.randomString(10),
+			TemplateBuild:       ts.randomString(10),
 		}
 		testTemplatePasswordEnabled = !testConfig.TemplateDisablePassword
 		testTemplateSSHKeyEnabled   = !testConfig.TemplateDisableSSHKey
@@ -27,13 +30,16 @@ func (ts *testSuite) TestStepRegisterTemplate_Run() {
 
 	testTemplate := &egoscale.Template{
 		BootMode:        &testConfig.TemplateBootMode,
+		Build:           &testConfig.TemplateBuild,
 		Checksum:        &testSnapshotChecksum,
 		DefaultUser:     &testConfig.TemplateUsername,
 		Description:     &testConfig.TemplateDescription,
+		Maintainer:      &testConfig.TemplateMaintainer,
 		Name:            &testConfig.TemplateName,
 		PasswordEnabled: &testTemplatePasswordEnabled,
 		SSHKeyEnabled:   &testTemplateSSHKeyEnabled,
 		URL:             &testSnapshotPresignedURL,
+		Version:         &testConfig.TemplateVersion,
 	}
 	ts.state.Put("snapshot_checksum", testSnapshotChecksum)
 	ts.state.Put("snapshot_url", testSnapshotPresignedURL)
@@ -73,6 +79,9 @@ func (ts *testSuite) TestStepRegisterTemplateWithEmptyFields_Run() {
 			TemplateDescription: "",
 			TemplateUsername:    "",
 			TemplateBootMode:    ts.randomString(10),
+			TemplateMaintainer:  "",
+			TemplateVersion:     "",
+			TemplateBuild:       "",
 		}
 		testTemplatePasswordEnabled = !testConfig.TemplateDisablePassword
 		testTemplateSSHKeyEnabled   = !testConfig.TemplateDisableSSHKey
@@ -83,13 +92,16 @@ func (ts *testSuite) TestStepRegisterTemplateWithEmptyFields_Run() {
 
 	testTemplate := &egoscale.Template{
 		BootMode:        &testConfig.TemplateBootMode,
+		Build:           nil,
 		Checksum:        &testSnapshotChecksum,
 		DefaultUser:     nil,
 		Description:     nil,
+		Maintainer:      nil,
 		Name:            &testConfig.TemplateName,
 		PasswordEnabled: &testTemplatePasswordEnabled,
 		SSHKeyEnabled:   &testTemplateSSHKeyEnabled,
 		URL:             &testSnapshotPresignedURL,
+		Version:         nil,
 	}
 	ts.state.Put("snapshot_checksum", testSnapshotChecksum)
 	ts.state.Put("snapshot_url", testSnapshotPresignedURL)
